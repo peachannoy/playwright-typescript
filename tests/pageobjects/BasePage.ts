86e6df5e161d0c0d590930b28b6aa207fb354779
peachannoy <david.spasow@gmail.com>
2023-01-30 15:05:35 +0100

    added-basepage-PO

--- tests/pageobjects/BasePage.ts
+++ tests/pageobjects/BasePage.ts
@@ -1,4 +1,28 @@
-class BasePage{
+import { Locator, Page } from "@playwright/test";
 
+export class BasePage {
+  private homeButton: Locator;
+  private ordersButton: Locator;
+  private cartButton: Locator;
+  private signOutButton: Locator;
+
+  constructor(private page: Page) {
+    this.homeButton = this.page.locator("[routerlink*='dashboard']");
+    this.ordersButton = this.page.locator("[routerlink*='myorders']");
+    this.cartButton = this.page.locator("[routerlink*='cart']");
+    this.signOutButton = this.page.locator("li:nth-child(4) button:nth-child(1)");
+  }
+
+  async gotoHome() {
+    await this.homeButton.click();
+  }
+  async gotoOrders() {
+    await this.ordersButton.click();
+  }
+  async gotoCart() {
+    await this.cartButton.click();
+  }
+  async signOut(){
+    await this.signOutButton.click();
+  }
 }
-module.exports = {BasePage}
--- tests/pageobjects/POManager.ts
+++ tests/pageobjects/POManager.ts
@@ -1,16 +1,19 @@
 const { DashboardPage } = require('./DashboardPage');
 const {LoginPage} = require('./LoginPage');
+import {BasePage} from './BasePage';
 
 export class POManager
 {
     page: any;
     loginPage: any;
     dashboardPage: any;
+    basePage: BasePage;
     constructor(page)
     {
         this.page = page;
         this.loginPage = new LoginPage(this.page);
         this.dashboardPage = new DashboardPage(this.page);
+        this.basePage = new BasePage(this.page);
     }
 
     getLoginPage()
