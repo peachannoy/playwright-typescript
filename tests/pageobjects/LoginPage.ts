ec68e6df363ae56baa002013be58139e72d393c7
peachannoy <david.spasow@gmail.com>
2023-01-31 12:50:16 +0100

    added-extensive-login-tests

--- tests/pageobjects/LoginPage.ts
+++ tests/pageobjects/LoginPage.ts
@@ -1,28 +1,48 @@
-class LoginPage{
-    page: any;
-    signInButton: any;
-    userName: any;
-    password: any;
+import { Locator, Page } from "@playwright/test";
+import { LoginResults } from "../utils/loginResults"; 
 
-    constructor(page)
+export class LoginPage{
+    userNameField: Locator;
+    passwordField: Locator;
+    signInButton: Locator;
+    missingEmail: Locator;
+    missingPassword: Locator;
+    invalidEmail: Locator;
+    incorrectData: Locator;
+
+    constructor(private page: Page)
      {
-        this.page = page;
+        this.userNameField = page.locator("#userEmail");
+        this.passwordField = page.locator("#userPassword");
         this.signInButton = page.locator("[value='Login']");
-        this.userName = page.locator("#userEmail");
-        this.password = page.locator("#userPassword");
+        this.missingEmail = page.getByText("*Email is required");
+        this.missingPassword = page.getByText("*Password is required");
+        this.invalidEmail = page.getByText("*Enter Valid Email");
+        this.incorrectData = page.getByText("Incorrect email or password.");
      }
 
-     async validLogin(username, password)
-     {
-        await this.userName.type(username);
-        await this.password.type(password);
-        await this.signInButton.click();
-        await this.page.waitForLoadState('networkidle');
-     }
-
-     async goTo(url)
-     {
-        await this.page.goto(url);
-     }
+     async login(username: string, password: string): Promise<LoginResults> {
+      await this.userNameField.type(username);
+      await this.passwordField.type(password);
+      await this.signInButton.click();
+      await this.page.waitForLoadState('networkidle');
+    
+      if (await this.missingEmail.isVisible()) {
+        return LoginResults.MISSINGEMAIL;
+      }
+    
+      if (await this.missingPassword.isVisible()) {
+         return LoginResults.MISSINGPASSWORD;
+      }
+    
+      if (await this.invalidEmail.isVisible()) {
+         return LoginResults.INVALIDEMAIL;
+      }
+    
+      if (await this.incorrectData.isVisible()) {
+         return LoginResults.INVALID;
+      }
+    
+      return LoginResults.VALID;
+    }
 }
-module.exports = {LoginPage}
